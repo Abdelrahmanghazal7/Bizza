ab3d822d15d401ec094e68fb5cf4b825fc56d438
Abdelrahman Ghazal <116028780+Abdelrahmanghazal7@users.noreply.github.com>
2022-12-08 19:36:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -94,6 +94,18 @@ setInterval(() => {
     }
 }, 5);
 
+document
+  .getElementById("dark-mode-btn")
+  .addEventListener("click", function (e) {
+    const toggler = document.body;
+    toggler.classList.toggle("dark-mode");
+    const target = e.target;
+    target.classList.toggle("fa-moon");
+    target.classList.toggle("fa-sun");
+  });
+
+
+
 function success() {
     swal({
         title: "Good job",
